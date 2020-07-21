c9a368e648b202c57c2571492f202c12e82e5998
Britta <50714303+DorkLordCthulhu@users.noreply.github.com>
2020-07-21 10:22:36 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package configPractice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        configPractice.TheReader x = new configPractice.TheReader();
+        x.readText();
+
+    }
+}
